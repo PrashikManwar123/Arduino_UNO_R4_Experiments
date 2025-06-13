b1f32b0f0f4695fd178e988ad76d945fef03a043
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-06-13 12:31:58 +0530

    Update 03_potentiometer_reading.ino

--- arduino-iot-experiments/03_potentiometer_reading/03_potentiometer_reading.ino
+++ arduino-iot-experiments/03_potentiometer_reading/03_potentiometer_reading.ino
@@ -3,8 +3,11 @@ void setup() {
 }
 
 void loop() {
-  int sensorValue = analogRead(A0);  // Read value from A0
+  int sensorValue = analogRead(A0);  // Read analog value from A0
+  // ADC has a resolution of 10 bits, so it produces values from 0 to 1023 (0 - 2^10)
+  // sensorValue will contain analog value between 0 and 1023
   Serial.println("Voltage across potentiometer is:");
   Serial.println((float(sensorValue)/1023)*5);       // Print the value to Serial Monitor
+  // Here, (float(sensorValue)/1023)*5 is the formula to convert digital value into a actual voltage values.
   delay(500);                        // Wait half a second
 }
