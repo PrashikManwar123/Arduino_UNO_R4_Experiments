206ee17154c319c26b98dfc157e82d4a33e2bd86
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-06-13 12:56:04 +0530

    Update 04_button_toggle_led.ino

--- arduino-iot-experiments/04_button_toggle_led/04_button_toggle_led.ino
+++ arduino-iot-experiments/04_button_toggle_led/04_button_toggle_led.ino
@@ -1,27 +1,30 @@
-const int buttonPin = 2;     // Button connected to pin 2
-const int ledPin = 13;       // LED connected to pin 13
+const int buttonPin = 2;     // Button connected to pin 2 it is set as constant 
+const int ledPin = 13;       // LED connected to pin 13 it is set as constant
 
-bool ledState = false;       // LED ON/OFF
-bool buttonPressed = false;  // Button press flag
+bool ledState = false;       // this var keeps the track whether the LED is ON/OFF
+bool buttonPressed = false;  // this var keeps the track whethter the button is pressed on not.
 
 void setup() {
-  pinMode(buttonPin, INPUT_PULLUP);  // Use internal pull-up resistor
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
+  pinMode(buttonPin, INPUT_PULLUP);  // INPUT_PULLUP activates the internal pullup resistor in the Arduino. This is done to avoid the use of external resistor.
+  // when buttonPressed is false -> pin reads high   
+  // when buttonPressed is true -> pin reads low
+  pinMode(ledPin, OUTPUT); 
+  digitalWrite(ledPin, LOW); //turns off the LED at the startup
   Serial.begin(9600);
 }
 
 void loop() {
   if (digitalRead(buttonPin) == LOW) {  // Button pressed
+    // implementing debouncing logic: 
     if (!buttonPressed) {
       delay(300); //this delay is introduced to prevent debouncing 
-      ledState = !ledState;  // Toggle LED
-      digitalWrite(ledPin, ledState ? HIGH : LOW);
+      ledState = !ledState;  // Toggles the current LED state
+      digitalWrite(ledPin, ledState ? HIGH : LOW); // Shows the output of ledState on LED connected to ledPin 
       Serial.print("LED is now ");
       Serial.println(ledState ? "ON" : "OFF");
-      buttonPressed = true;
+      buttonPressed = true; //prevents the led from repeatedly toggling while the buttonPressed in true
     }
   } else {
-    buttonPressed = false; // Reset flag when button is released
+    buttonPressed = false; // Reset buttonPressed when button is released
   }
 }
