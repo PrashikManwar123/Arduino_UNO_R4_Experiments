86c06ad3ca1c6470e32594b96fc4cb58c57c671b
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-06-13 13:27:33 +0530

    Update 05_potentiometer_led_control.ino

--- arduino-iot-experiments/05_potentiometer_led_control/05_potentiometer_led_control.ino
+++ arduino-iot-experiments/05_potentiometer_led_control/05_potentiometer_led_control.ino
@@ -1,3 +1,5 @@
+// Pot has 3 pins - Vcc, signal and GND
+
 const int potPin = A0;     // Potentiometer connected to A0
 const int ledPin = 9;      // LED connected to PWM pin 9
 
@@ -7,12 +9,12 @@ void setup() {
 }
 
 void loop() {
-  int potValue = analogRead(potPin);          // Read value (0–1023)
-  int brightness = map(potValue, 0, 1023, 0, 255); // Scale to 0–255 for PWM
-  analogWrite(ledPin, brightness);            // Set LED brightness
+  int potValue = analogRead(potPin);          // The pot reading is stored in variable potValue. It reads value (0–1023)
+  int brightness = map(potValue, 0, 1023, 0, 255); // analogWrite accepts the value from 0-255 only. Hence we scaled it as shown and stored the scaled value in var brightness.
+  analogWrite(ledPin, brightness);            
   Serial.print("Potentiometer: ");
-  Serial.print(potValue);
+  Serial.print(potValue); // shows value from 0-1023 (2^10 levels)
   Serial.print(" -> Brightness: ");
-  Serial.println(brightness);
+  Serial.println(brightness); // shows value from 0-255 (2^8 levels)
   delay(200);
 }
