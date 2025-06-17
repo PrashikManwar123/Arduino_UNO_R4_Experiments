d2f93d5b0bed8136fd9c0c9eb64bb0664b8085cb
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-06-17 12:01:51 +0530

    Update 07_lcd_hello_world.ino

--- arduino-iot-experiments/07_lcd_hello_world/07_lcd_hello_world.ino
+++ arduino-iot-experiments/07_lcd_hello_world/07_lcd_hello_world.ino
@@ -1,5 +1,5 @@
-#include <Wire.h>
-#include "Waveshare_LCD1602_RGB.h"
+#include <Wire.h> // Library used for I2C communication
+#include "Waveshare_LCD1602_RGB.h" // Library for LCD 
 
 // Create LCD object with 16 columns and 2 rows
 Waveshare_LCD1602_RGB lcd(16, 2);
@@ -13,5 +13,5 @@ void setup() {
 }
 
 void loop() {
-  // Nothing to do here
+  // empty
 }
