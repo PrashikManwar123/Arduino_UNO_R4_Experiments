4df22b97cc687c43938f6e3c570cfaeaf7a6d9ae
prashikmanwar911 <167643811+PrashikManwar123@users.noreply.github.com>
2025-06-17 11:57:45 +0530

    Update 06_interrupt_led_toggle.ino

--- arduino-iot-experiments/06_interrupt_led_toggle/06_interrupt_led_toggle.ino
+++ arduino-iot-experiments/06_interrupt_led_toggle/06_interrupt_led_toggle.ino
@@ -2,9 +2,12 @@ volatile bool ledState = false;  // must be volatile for use in ISR
 
 void setup() {
   pinMode(8, OUTPUT);            // LED pin
-  pinMode(2, INPUT_PULLUP);      // Button pin with internal pull-up
+  pinMode(2, INPUT_PULLUP);      // Button pin with internal pull-up. The pin reads HIGH normally, and becomes LOW when button is pressed.
 
-  attachInterrupt(digitalPinToInterrupt(2), turnOnLED, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), turnOnLED, FALLING); 
+  // digitalPinToInterrupt(2) - initialises pin 2 to interrupt
+  // turnOnLED - interrupt function to call 
+  // FALLING - indicates that the interrupt will be executed during falling edge of pwm, i.e when pin2 goes from high to low 
   Serial.begin(9600);
 }
 
